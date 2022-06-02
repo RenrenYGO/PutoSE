2b7883dc76b7ad280fc08b9ad38d718e717786d9
RenrenYGO <cardo85@mail.com>
2022-06-02 14:22:29 +0800

    added post display template

---
+++ application/views/pages/showpost.php
@@ -0,0 +1,37 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BSCS-3C | Home</title>
+    <!--Bootstrap 5 elements link-->
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/images/favicon.png">
+    <!-- Remix icons -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <!-- Swiper.js styles -->
+    <link rel="stylesheet" href="<?php echo base_url('/assets/css/swiper-bundle.min.css')?>" />
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css')?>">
+</head>
+<body> 
+
+<div class="castoria-box">
+    <div class="castoria-img-box">
+        <img src="<?php echo base_url('/assets/images/featured/Debug01.png')?>" width="90" height="105" >
+        <span>Castoria</span>
+    <div>
+        <span>Banner Announcement</span>   
+    </div>
+        <span>I'll be present in the upcoming banner, so make sure to save up. See you next month!</span>
+    </div>
+</div>
+
+    <div class="mb-6">
+        <input type="comment" class="form-control" placeholder="Comment your thoughts..." name="comment">
+        <button type="submit" class="btn btn-post">Comment</button>
+    </div>
