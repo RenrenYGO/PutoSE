35f4189c3dbaf048b7106221d24bcee47b53098d
RenrenYGO <cardo85@mail.com>
2022-06-08 23:28:50 +0800

    aesthetic tweaks

--- application/views/pages/edit_profile.php
+++ application/views/pages/edit_profile.php
@@ -3,7 +3,7 @@
     <?php echo form_open_multipart('edit_profile/profile_edit');?>
     <input type="hidden" name="id" value="<?php echo $this->session->userdata('user')['id'];?>">
     <div class="box mt-5 col-6 mx-auto border border-2">
-        <h2 class="text-center mt-2 pt-3">Edit Profile</h2>
+        <h2 class="text-center mt-2 pt-3"><b>Edit Profile</b></h2>
         <div class="px-5 mx-3 pt-4 mt-3">
             <div class="form-group mb-2">
                 <input type="text" class="form-control" name="name" placeholder="Edit Username" value="<?php echo $this->session->userdata('user')['name'];?>">
@@ -16,7 +16,7 @@
                 <input type="file" name="profile_picture" size="200">
             </div>
             <div class="ms-auto">
-                <button type="submit" class="btn btn-default mt-2">Submit</button>
+                <button type="submit" class="btn btn-custom">Submit</button>
             </div>
         </div>
     </div>
