e4ce0e24d4eec06c9370613fd9b5630ef633c790
richard <rasunga30@gmail.com>
2022-06-28 18:47:37 +0800

    fix: page title UI

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -6,19 +6,20 @@
 
             <div class="featured-content posts d-grid">
 
-                <div class="d-flex">
-                <div class="container text-dark mx-5 px-5">
-                    <h1><?php echo $pagetit; ?></h1>
+                <div class="d-flex justify-content-between align-items-center">
+                <div class="text-dark">
+                    <h2><?php echo $pagetit; ?></h2>
                 </div>
-                <div class="input-group">
+
+                <div class="input-group d-flex justify-content-center">
                     <form action = "<?php echo site_url('posts/skeyword/');?>" method="post">
                         <input type="text" name="title" placeholder="&#xf002 Search..." style="font-family:FontAwesome" id="search" class="bg-light form-control">
                     </form>
                 </div>
                 
-                <div class="container text-dark mx-5 px-5">
+                <div class="text-dark" >
                     <?php if(isset($_SESSION['user']['name'])):?>
-                        <button class="btn sign-up-btn fancy-border screen-sm-hidden " >
+                        <button class="btn sign-up-btn fancy-border screen-sm-hidden " style="width:11em">
                             <a class="justify-content-center"href="<?php echo base_url('posts/create');?>" >
                                 <span>Create Post + </span>
                             </a>
